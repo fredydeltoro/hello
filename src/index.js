ba4de6068de2263ca6391713880e6a74367a7355
Alfredo Monroy <fredydeltoro@gmail.com>
2020-02-09 17:39:44 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// prueba de edicion
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
